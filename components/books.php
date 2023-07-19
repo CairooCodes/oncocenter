fe40c41ca948de70253b24fe328b55530af95663
Cairo <cairofelipedev@gmail.com>
2023-07-19 12:36:06 -0300

    Home redes sociais

--- components/books.php
+++ components/books.php
@@ -1,5 +1,5 @@
 <section class="swiper swiper_books max-w-screen-xl mx-auto lg:p-10 p-2">
-  <h2 class="mb-6 text-3xl font-extrabold tracking-tight text-center text-color1 lg:mb-8 lg:text-3xl">
+  <h2 class="mb-6 text-3xl font-extrabold tracking-tight text-center text-color1 lg:text-3xl">
     Livros</h2>
   <div class="swiper-wrapper lg:mt-6">
     <?php $stmt = $DB_con->prepare("SELECT * FROM books order by id desc");
@@ -13,7 +13,7 @@
             <img class="rounded-md h-full w-full" src="./admin/uploads/books/<?php echo $img; ?>">
           </div>
           <div class="flex justify-center my-4">
-            <a target="blank" href="<?php echo $link?>" class="mb-4 text-white bg-color1 focus:ring-4 rounded-md font-bold text-md px-5 py-2 text-center">Baixar</a>
+            <a target="blank" href="<?php echo $link ?>" class="mb-4 text-white bg-color1 focus:ring-4 rounded-md font-bold text-md px-5 py-2 text-center">Baixar</a>
           </div>
         </div>
       </div>
---
+++ informativos.php
@@ -0,0 +1,94 @@
+<?php
+require "db_config.php";
+require "config/helper.php";
+require "config/url.class.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <?php include "components/heads.php"; ?>
+</head>
+
+<body>
+  <?php include "components/navbar.php"; ?>
+  <div class="mx-auto max-w-6xl pt-2">
+    <div class="lg:p-10 p-2">
+      <h2 class="mb-6 text-3xl font-extrabold tracking-tight text-center text-color1 lg:mb-8 lg:text-3xl">
+        Informativos</h2>
+      <div class="max-w-screen-md mx-auto px-7 py-3 bg-white rounded-lg">
+        <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-transparent" data-inactive-classes="text-gray-500">
+          <h3 id="accordion-flush-heading-1">
+            <button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-color1 border-b border-gray-200" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
+              <span>Oncocenter Informa</span>
+              <svg data-accordion-icon="" class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </button>
+          </h3>
+          <div id="accordion-flush-body-1" class="" aria-labelledby="accordion-flush-heading-1">
+            <div class="py-5 border-b border-gray-200">
+              <p class="mb-2 text-gray-500">A Clínica de RadioOncologia informa aos nossos pacientes e familiares como viver uma vida com mais qualidade e saúde</p>
+            </div>
+          </div>
+          <h3 id="accordion-flush-heading-2">
+            <button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-color1  border-b border-gray-200" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
+              <span>Câncer de mama</span>
+              <svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </button>
+          </h3>
+          <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
+            <div class="py-5 border-b border-gray-200">
+              <a target="blank" href="./assets/docs/pdf/cancer-de-mama.pdf" class="text-color1 hover:underline">Leia o conteúdo aqui</a>
+            </div>
+          </div>
+          <h3 id="accordion-flush-heading-3">
+            <button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-color1  border-b border-gray-200" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
+              <span>Câncer de mama: Informações aos pacientes e familiares</span>
+              <svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </button>
+          </h3>
+          <div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
+            <div class="py-5 border-b border-gray-200">
+              <a target="blank" href="./assets/docs/pdf/cancer-de-mama-info.pdf" class="text-color1 hover:underline">Leia o conteúdo aqui</a>
+            </div>
+          </div>
+          <h3 id="accordion-flush-heading-4">
+            <button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-color1  border-b border-gray-200" data-accordion-target="#accordion-flush-body-4" aria-expanded="false" aria-controls="accordion-flush-body-4">
+              <span>Vitamina D</span>
+              <svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </button>
+          </h3>
+          <div id="accordion-flush-body-4" class="hidden" aria-labelledby="accordion-flush-heading-4">
+            <div class="py-5 border-b border-gray-200">
+              <a target="blank" href="./assets/docs/pdf/vitamina-d.pdf" class="text-color1 hover:underline">Leia o conteúdo aqui</a>
+            </div>
+          </div>
+          <h3 id="accordion-flush-heading-5">
+            <button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-color1  border-b border-gray-200" data-accordion-target="#accordion-flush-body-5" aria-expanded="false" aria-controls="accordion-flush-body-5">
+              <span>Check ups e exames de rotina são mesmo necessários?</span>
+              <svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+              </svg>
+            </button>
+          </h3>
+          <div id="accordion-flush-body-5" class="hidden" aria-labelledby="accordion-flush-heading-5">
+            <div class="py-5 border-b border-gray-200">
+              <a target="blank" href="./assets/docs/pdf/checkup.pdf" class="text-color1 hover:underline">Leia o conteúdo aqui</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include "components/footer.php"; ?>
+
+</body>
+
+</html>
